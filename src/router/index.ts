793cf874e814bbb83f510af471fc8d6d84edaad1
Gabafle <gabriel.peter@hotmail.fr>
2025-06-06 13:51:03 +0100

    Commit re build with router change

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 import HomePages from '@/pages/homePages.vue'
 import monProfil from '@/pages/monProfil.vue'
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
